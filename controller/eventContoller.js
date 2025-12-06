21c252c793f67bf403259e2df1195c46b99267bc
Jaiduino <jayeshbaraskar@Jayeshs-MacBook-Air.local>
2025-12-07 00:34:37 +0530

    image_url issue fixed

--- controller/eventContoller.js
+++ controller/eventContoller.js
@@ -2,10 +2,10 @@ const { sendResponse } = require('../_helpers/responseHelper');
 const eventService = require('../services/eventService');
 const eventImageService = require('../services/eventImageService');
 
-async function createEvent(req, res) {
-
+async function createEvent(req, res,next) {
+      
     try {
-        const event = await eventService.createEvent(req, res)
+        const event = await eventService.createEvent(req.body)
 
         return sendResponse(res, 201, "Event created", event);
     } catch (error) {
